5a63ddc54f92b90a213c69a2a6cb4efeba259bde
SpatLyu <lyu.geosocial@gmail.com>
2025-06-11 16:58:15 +0800

    resolve R CMD check NOTE on `runif` being undefined

--- R/gen_dummy_data.R
+++ R/gen_dummy_data.R
@@ -6,12 +6,12 @@
 #' @export
 gen_dummy_data <- function(num_samples = 10000, random_state = 1) {
     set.seed(random_state)
-    x3 <- runif(n = num_samples)
-    x0 <- runif(n = num_samples) + 3 * x3
-    x2 <- runif(n = num_samples) + 6 * x3
-    x1 <- runif(n = num_samples) + 3 * x0 + 2 * x2
-    x5 <- runif(n = num_samples) + 4 * x0
-    x4 <- runif(n = num_samples) + 8 * x0 - 1 * x2
+    x3 <- stats::runif(n = num_samples)
+    x0 <- stats::runif(n = num_samples) + 3 * x3
+    x2 <- stats::runif(n = num_samples) + 6 * x3
+    x1 <- stats::runif(n = num_samples) + 3 * x0 + 2 * x2
+    x5 <- stats::runif(n = num_samples) + 4 * x0
+    x4 <- stats::runif(n = num_samples) + 8 * x0 - 1 * x2
     X  <- data.frame(x0, x1, x2, x3, x4, x5)
     return(X)
 }
